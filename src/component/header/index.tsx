bd6cbfc6752c2f09a98e53898f6ae64c3fda8e2b
easymean <m2162003@gmail.com>
2021-03-26 01:20:17 +0900

    feat: add header

--- src/component/header/index.tsx
+++ src/component/header/index.tsx
@@ -2,7 +2,32 @@ import React from "react";
 import * as S from "./styles";
 
 const Header = () => {
-  return <></>;
+  return (
+    <>
+      <S.Header>
+        <S.DesktopLink to="/">
+          <S.EmartLogo>이마트</S.EmartLogo>
+        </S.DesktopLink>
+
+        <S.LinkContainer>
+          <S.DesktopLink to={"/blossom"}>
+            <S.LinkText>블라썸</S.LinkText>
+          </S.DesktopLink>
+          <S.DesktopLink to="/xwiki">
+            <S.LinkText>XWiki</S.LinkText>
+          </S.DesktopLink>
+
+          <S.DesktopLink to="/jira">
+            <S.LinkText>JIRA</S.LinkText>
+          </S.DesktopLink>
+
+          <S.DesktopLink to="/confl">
+            <S.LinkText>Confluence</S.LinkText>
+          </S.DesktopLink>
+        </S.LinkContainer>
+      </S.Header>
+    </>
+  );
 };
 
 export default Header;
--- src/component/header/styles.ts
+++ src/component/header/styles.ts
@@ -1,1 +1,40 @@
+import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { BREAKPOINT } from "@asset/constant";
+
+export const Header = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+
+  width: 100%;
+  height: 7rem;
+  background-color: grey;
+
+  padding: 0rem 3rem;
+
+  @media only screen and (min-width: ${BREAKPOINT}px) {
+    height: 10rem;
+  }
+`;
+
+export const LinkContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: flex-end;
+
+  width: 30%;
+`;
+
+export const EmartLogo = styled.image``;
+
+export const LinkText = styled.p`
+  color: white;
+  text-align: center;
+`;
+
+export const DesktopLink = styled(Link)`
+  display: flex;
+`;
