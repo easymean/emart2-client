ab87fed3fc0ef85d8945219ca571a862a94bb66c
easymean <m2162003@gmail.com>
2021-03-26 01:21:10 +0900

    feat: add external url router

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,9 @@ import MainPage from "@view/main";
 import BoardPage from "@view/board";
 import Layout from "@component/common/layout";
 import GlobalStyles from "@component/common/global-styles";
+import { BLOSSOM_URL, CONFL_URL, JIRA_URL, XWIKI_URL } from "@asset/constant";
 
-const App = () => {
+const App: React.FC = () => {
   return (
     <>
       <GlobalStyles />
@@ -14,7 +15,36 @@ const App = () => {
         <Layout>
           <Switch>
             <Route path="/board/:boardId" component={BoardPage} />
+            <Route
+              path="/blossom"
+              component={() => {
+                window.location.href = BLOSSOM_URL;
+                return null;
+              }}
+            />
+            <Route
+              path="/xwiki"
+              component={() => {
+                window.location.href = XWIKI_URL;
+                return null;
+              }}
+            />
+            <Route
+              path="/jira"
+              component={() => {
+                window.location.href = JIRA_URL;
+                return null;
+              }}
+            />
+            <Route
+              path="/confl"
+              component={() => {
+                window.location.href = CONFL_URL;
+                return null;
+              }}
+            />
             <Route path="/" component={MainPage} />
+
             <Redirect from="*" to="/" />
           </Switch>
         </Layout>
