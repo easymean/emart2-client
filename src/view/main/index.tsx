dec280d13109b8ead2af77b9900eb495804f5513
easymean <m2162003@gmail.com>
2021-03-26 01:20:41 +0900

    chore: remove console.log

--- src/view/main/index.tsx
+++ src/view/main/index.tsx
@@ -5,7 +5,6 @@ import Search from "@container/search";
 import Menu from "@container/menu";
 
 const MainPage = () => {
-  console.log("hello");
   return (
     <>
       <S.Title>이마트 2팀에 오신걸 환영합니다.</S.Title>
