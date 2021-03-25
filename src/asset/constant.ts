90a44acb2d07a2edae6677ba21030eb5a7fdcfba
easymean <m2162003@gmail.com>
2021-03-26 01:19:52 +0900

    feat: asset 추가

---
+++ src/asset/constant.ts
@@ -0,0 +1,10 @@
+export const BREAKPOINT = 640;
+
+export const STATIC_URL = {};
+
+export const THEME_COLOR = {};
+
+export const BLOSSOM_URL = "https://blossom.shinsegae.com/";
+export const XWIKI_URL = "";
+export const JIRA_URL = "https://jira.sinc.co.kr/";
+export const CONFL_URL = "https://confl.sinc.co.kr/";
