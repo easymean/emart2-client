691f41c8b2586730287247c9344ce0ce91c6e51a
easymean <m2162003@gmail.com>
2021-03-28 19:55:57 +0900

    chore: rename menu to maincategory

--- src/container/menu/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Menu = () => {
-  return <></>;
-};
-
-export default Menu;
--- src/view/main/index.tsx
+++ src/view/main/index.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import * as S from "./styles";
 
 import MainSearch from "@/container/main-search";
-import Menu from "@container/menu";
+import MainCateogory from "@/container/main-cateogory";
 
 const MainPage = () => {
   return (
     <S.MainContainer>
       <S.Title>이마트 2팀에 오신걸 환영합니다.</S.Title>
-      <MainSearch/>
-      <Menu />
+      <MainSearch />
+      <MainCateogory />
     </S.MainContainer>
   );
 };
