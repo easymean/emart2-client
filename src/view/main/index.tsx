53eb991fb41dbd28a5e497596dfe5788e8161ebb
easymean <m2162003@gmail.com>
2021-03-29 02:08:10 +0900

    chore: rename

--- src/view/main/index.tsx
+++ src/view/main/index.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import * as S from "./styles";
 
 import MainSearch from "@/container/main-search";
-import MainCateogory from "@/container/main-cateogory";
+import MainCateogoryList from "@/container/main-cateogory-list";
 
 const MainPage = () => {
   return (
     <S.MainContainer>
       <S.Title>이마트 2팀에 오신걸 환영합니다.</S.Title>
       <MainSearch />
-      <MainCateogory />
+      <MainCateogoryList />
     </S.MainContainer>
   );
 };
