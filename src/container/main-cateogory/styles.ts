767f99c3808520b13110a3d558ca6a62e1d1a440
easymean <m2162003@gmail.com>
2021-03-28 19:55:06 +0900

    feat: add models

---
+++ src/container/main-cateogory/styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const CategoryContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-itetms: center;
+
+  margin: 5rem 0rem;
+`;
---
+++ src/module/model/cateoryModel.ts
@@ -0,0 +1,4 @@
+export default interface CategoryModel {
+  id: number;
+  name: string;
+}
