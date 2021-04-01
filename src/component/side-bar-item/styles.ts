1751cd40ccfac649f7bff3b91a5cd41569337500
easymean <m2162003@naver.com>
2021-04-01 12:54:34 +0900

    fix: fix sidebaritem layout

--- src/component/side-bar-item/styles.ts
+++ src/component/side-bar-item/styles.ts
@@ -4,24 +4,15 @@ import { THEME_COLOR } from "@asset/constant";
 
 export const SideBarItem = styled(Link)`
   width: 100%;
-  height: 5rem;
-  // display: flex;
-  // margin-right: 1rem;
-  // justify-content: flex-start;
-  // align-items: flex-start;
+  height: 6rem;
   text-decoration: none;
 
   padding: 1rem 2rem;
-  font-size: 1.2rem;
+  font-size: 2rem;
+  font-wieght: 500;
   color: ${THEME_COLOR.BLACK};
 
   &:hover {
     background-color: whitesmoke;
   }
-`;
-
-export const SideBarTitle = styled.div`
-  font-size: 2rem;
-  font-weight: 300;
-  font-color: ${THEME_COLOR.BLACK};
 `;
