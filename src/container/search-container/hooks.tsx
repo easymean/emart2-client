830dc1c0ce7e62e5c2131b29a3b29de0968790f4
easymean <m2162003@gmail.com>
2021-04-18 17:57:26 +0900

    feat: search website api by keyword

---
+++ src/container/search-container/hooks.tsx
@@ -0,0 +1,56 @@
+import React, { useCallback, useEffect, useState } from "react";
+import siteAPI from "@api/website";
+import SiteModel from "@model/siteModel";
+import HashtagModel from "@model/hashtagModel";
+
+const useDebounce = (state: string, delay: number): string => {
+  const [debounceState, setDebounceState] = useState(state);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebounceState(state);
+    }, delay);
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [state, delay]);
+  return debounceState;
+};
+
+export const useSearchBar = () => {
+  const [keyword, setKeyword] = useState("");
+  const [siteList, setSiteList] = useState([] as SiteModel[]);
+
+  const onKeyup = useCallback((e: React.KeyboardEvent<HTMLInputElement>) => {
+    setKeyword(e.currentTarget.value);
+  }, []);
+
+  const debounceKeyword = useDebounce(keyword, 1000);
+
+  const searchSiteList = useCallback(
+    (keyword: string) => async () => {
+      if (!keyword) return;
+
+      const siteList = await siteAPI.searchSitebyKeyword(keyword);
+      setSiteList(siteList);
+    },
+    [debounceKeyword]
+  );
+
+  useEffect(() => {
+    if (!debounceKeyword) return;
+    searchSiteList(debounceKeyword)();
+  }, [debounceKeyword]);
+
+  return {
+    siteList,
+    onKeyup,
+  };
+};
+
+export const useHashtag = () => {
+  const [hashtags, setHashtags] = useState([] as HashtagModel[]);
+
+  return { hashtags };
+};
