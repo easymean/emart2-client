a0c57095ead7510797b70f50bff413c687950cb6
easymean <m2162003@naver.com>
2021-09-06 17:41:06 +0900

    feat: custom alert

---
+++ emart2/src/component/alert/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { AlertProps } from "./type";
+import * as S from "./styles";
+import { useHistory } from "react-router-dom";
+import { Modal } from "../common/modal/styles";
+
+const Alert = ({ redirect, pop, message }: AlertProps) => {
+  const history = useHistory();
+  const onClick = () => {
+    if (redirect) {
+      window.location.href = redirect;
+    } else if (pop) {
+      history.goBack();
+    }
+  };
+  return (
+    <Modal>
+      <S.Button onClick={onClick}>확인</S.Button>
+    </Modal>
+  );
+};
+export default Alert;
