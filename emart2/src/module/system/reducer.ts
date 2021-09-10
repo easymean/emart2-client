05d7bcf912c67232ccdcf90167692093921ed965
easymean <m2162003@naver.com>
2021-09-10 18:00:31 +0900

    fix: fix saga

---
+++ emart2/src/module/system/reducer.ts
@@ -0,0 +1,2 @@
+const categoryReducer;
+export default categoryReducer;
--- emart2/src/module/system/types.ts
+++ emart2/src/module/system/types.ts
@@ -1,2 +1,12 @@
-const categoryReducer;
-export default categoryReducer;
+import { CategoryModel } from "@/model/cateoryModel";
+import { AxiosError } from "axios";
+import { ActionType } from "typesafe-actions";
+import * as actions from "./actions";
+
+export type CategoryAction = ActionType<typeof actions>;
+
+export type CategoryState = {
+  loading: boolean;
+  error: AxiosError | null;
+  data: CategoryModel[] | null;
+};
